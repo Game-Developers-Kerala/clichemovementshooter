765ab284fbb480b8c7a61aca5676cf00f7814561
AnnanDistance <tripleprivate5@proton.me>
2022-11-13 02:33:04 +0530

    added walljumping

--- scripts/player.gd
+++ scripts/player.gd
@@ -4,14 +4,16 @@ enum movestates {none,ground,wall,air,grapple}
 var movestate = movestates.ground
 
 var mouse_sens = 0.1
-const GRAPPLESPEED = 40
+const GRAPPLESPEED = 30
 const GRAPPLEAIRCONTROL = 10
 const AIRCONTROL = 15
+var air_accel = 0.0
+const AIRACCEL = 10
+const AIRACCEL_LERPRATE = 100
 const GRAPPLEACCEL = 20
 const GROUNDACCEL = 10
-const AIRACCEL = 10
 const GRAVITY = 20
-const SLIDESPEED = 10
+const SLIDESPEED = 20
 const FRICTION_DAMPING = 0.9
 
 var velocity : Vector3 = Vector3.ZERO
@@ -23,9 +25,11 @@ var grappling = false
 var braking = false
 var jumping = false
 var on_wall = false
-var in_slide_raw = false
+var wallsidecheck = false
 var in_slide = false
+var in_jump = false
 var last_velocity = Vector3.ZERO
+var air_auto_dir = false
 
 var health := 100
 
@@ -45,11 +49,10 @@ func _input(event):
 
 func _unhandled_key_input(event):
 	if event.is_action_pressed("slide"):
-		in_slide_raw = true
 		in_slide = true
 	if event.is_action_released("slide"):
 		in_slide = false
-		in_slide_raw = false
+
 
 #====================================
 
@@ -62,7 +65,7 @@ func _physics_process(delta):
 	var grapplelength = 0.0
 	
 	set_move_state()
-	
+
 	match movestate:
 		movestates.none:
 			velocity = lerp(velocity,Vector3.ZERO,GROUNDACCEL*delta)
@@ -86,7 +89,12 @@ func _physics_process(delta):
 			velocity = lerp(velocity,grapplevec*speed+aircontrol*GRAPPLEAIRCONTROL,GRAPPLEACCEL*delta)
 			snap = Vector3.ZERO
 		movestates.air:
-			dir = Vector3(in_dir.x,0,in_dir.y).normalized().rotated(Vector3.UP,global_rotation.y)
+			if in_dir.length():
+				dir = Vector3(in_dir.x,0,in_dir.y).normalized().rotated(Vector3.UP,global_rotation.y)
+				air_auto_dir = false
+			else:
+				if air_auto_dir:
+					dir = (velocity*Vector3(1,0,1)).normalized()
 			velocity.x = lerp(velocity.x,dir.x*AIRCONTROL,AIRACCEL*delta)
 			velocity.z = lerp(velocity.z,dir.z*AIRCONTROL,AIRACCEL*delta)
 			velocity.y -= GRAVITY*delta
@@ -105,8 +113,12 @@ func _physics_process(delta):
 				snap = -floornorm
 			else:
 				snap = Vector3.ZERO
-
+	if Input.is_action_pressed("jump") and !jumping and on_wall:
+		walljump()
+	
 	velocity = move_and_slide_with_snap(velocity,snap,Vector3.UP,true)
+	var lookdir = (velocity.rotated(Vector3.UP,PI*0.5)*Vector3(1,0,1)).normalized()
+	$wallsidecheckarea.look_at($wallsidecheckarea.global_translation+lookdir,Vector3.UP)
 	last_velocity = velocity
 	jumping = false
 
@@ -117,43 +129,55 @@ func _physics_process(delta):
 	if Input.is_action_pressed("use"):
 		shoot_rail()
 	if Input.is_action_just_pressed("ui_page_down"):
-		print("Wall area overlapping bodies:",$WallArea.get_overlapping_bodies())
+		pass
 	$Label.text = movestates.keys()[movestate]
-	$Label.text +="\nInput Slide:"+str(in_slide)
+	$Label.text = "\nWallray colliding:"+str(wallsidecheck)
 
 func set_move_state():
 	if grappling:
-		movestate = movestates.grapple
+		change_movestate(movestates.grapple)
 		return
 	if is_on_floor():
 		match movestate:
 			movestates.none:
 				if jumping:
-					movestate = movestates.air
+					change_movestate(movestates.air)
 					return
 				if velocity.length()>0.001 and in_slide:
-					movestate = movestates.ground
+					change_movestate(movestates.ground)
 				return
 			movestates.ground:
 				if jumping:
-					movestate = movestates.air
+					change_movestate(movestates.air)
 					return
 				if !in_slide:
-					movestate = movestates.none
+					change_movestate(movestates.none)
 					return
 			_:
 				if in_slide:
-					movestate = movestates.ground
+					change_movestate(movestates.ground)
 					return
-				movestate = movestates.none
+				change_movestate(movestates.none)
 		return
 	if on_wall:
 		if in_slide:
-			movestate = movestates.wall
+			change_movestate(movestates.wall)
 		else:
-			movestate = movestates.air
+			change_movestate(movestates.air)
 		return
-	movestate = movestates.air
+	change_movestate(movestates.air)
+
+func change_movestate(to_state,args:={}):
+	match to_state:
+		movestates.air:
+			if movestate == movestates.ground and !jumping:
+				air_auto_dir = true
+			if args.has("auto_dir"):
+				air_auto_dir = true
+			air_accel = 4.0
+		_:
+			air_auto_dir = false
+	movestate = to_state
 
 func shoot():
 	if !$RocketCooldown.is_stopped():
@@ -167,6 +191,18 @@ func shoot():
 
 func get_hit(arg):
 	pass
+
+
+func walljump():
+	jumping = true
+	snap = Vector3.ZERO
+	var perp_vec = -$wallsidecheckarea.global_transform.basis.z*Vector3(1,0,1)
+	if wallsidecheck:
+		perp_vec = $wallsidecheckarea.global_transform.basis.z*Vector3(1,0,1)
+	print("perpvec:",perp_vec)
+	velocity.y = 0.0
+	velocity += Vector3.UP*6+perp_vec*10
+	change_movestate(movestates.air,{auto_dir=true})
 
 func grapple():
 	if grappling:
@@ -210,3 +246,12 @@ func _on_WallArea_body_entered(body):
 func _on_WallArea_body_exited(body):
 	if !$WallArea.get_overlapping_bodies():
 		on_wall = false
+
+
+func _on_wallsidecheckarea_body_entered(body):
+	wallsidecheck = true
+
+
+func _on_wallsidecheckarea_body_exited(body):
+	if !$wallsidecheckarea.get_overlapping_bodies():
+		wallsidecheck = false
